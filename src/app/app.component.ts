38cdca7c703930e50f1d6b1879e37651065b1fbc
swati.inje@greatmanagerinstitute.com <swati.inje@greatmanagerinstitute.com>
2023-11-06 09:33:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'priceSwitchPro';
+  toggle: string = "Dollar";
+
+  toggleCurrency(event: any) {
+    const selectedValue = event.target.getAttribute('value');
+
+    console.log(selectedValue);
+
+    this.toggle = selectedValue;
+  }
 }
+
+
